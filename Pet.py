f43017db5941097e40291193c4451ba6d6e07bfd
ronab07 <ronabaum06@gmail.com>
2024-09-17 13:32:18 +0300

    Update Pet.py

--- Pet.py
+++ Pet.py
@@ -1,14 +1,14 @@
 from enum import Enum
 class states(Enum):
-    Baby,
-    Child, 
-    Teenager,
-    Adult,
-    Senior,
-    Special
+    Baby = 1
+    Child = 2
+    Teenager = 3
+    Adult = 4
+    Senior = 5
+    Special = 6
     
 class Pet():
-    def __init__(self, state, hunger, happiness, training, sickness, states.state):
+    def __init__(self, state: States, hunger: int, happiness: int, training: int, sickness: int):
         self.counter = 0
         self.state = state
         self.hunger = hunger
