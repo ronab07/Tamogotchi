e4865350ce8cf9e17b561c9a035f827523ebb599
tamar88 <tamar.levko@gmail.com>
2024-09-26 17:46:38 +0300

    Update Pet.py

--- Pet.py
+++ Pet.py
@@ -9,41 +9,55 @@ class States(Enum):
     Dead = 7
     
 class Pet:
-    def __init__(self, state: States, hunger: int, happiness: int, training: int, sickness: int, weight: int, name: str):
+    def __init__(self, name: str):
         self.counter = 0
-        self.state = state
-        self.hunger = hunger
-        self.happiness = happiness
-        self.training = training
-        self.sickness = sickness
+        self.state = "Baby"
+        self.hunger = 30
+        self.happiness = 100
+        self.training = 20
+        self.sickness = 0
         self.MAX_LIMIT_MEAL = 3
-        self.weight = weight
+        self.weight = 5
         self.age = 0
         self.name = name
+        self.SNACK_COUNT = 0
+        self.EX_COUNT = 0
 
     def eat(self):
-        pass
-        # self.update_state()
+        if MAX_LIMIT_MEAL != 0:
+            self.hunger += 10
+            MAX_LIMIT_MEAL -=1
+            counter +=1
+        self.update_state()
 
     def play(self):
-        self.happiness *= 1.5
+        self.happiness += 15
+        counter +=1
         self.update_state()
 
 
     def sleep(self):
-        self.happiness *= 1.1
-        self.sickness *= 0.75
+        self.happiness += 5
+        self.sickness -= 7
+        counter +=1
         self.update_state()
 
 
     def exercise(self):
-        pass
-        # self.update_state()
+        EX_COUNT +=1
+        counter +=1
+        if EX_COUNT>3:
+            self.sickness +=15
+        self.update_state()
 
 
     def snack(self):
-        self.happiness *= 1.5
-        self.hunger *= 1.25
+        SNACK_COUNT += 1
+        self.happiness += 8
+        self.hunger += 5
+        counter +=1
+        if SNACK_COUNT>3:
+            self.sickness += 7
         self.update_state()
 
 
